5b72af5f2d3b416d3df9c78ce638e97343354763
Prakash K <prakash@HQ.CLI.IN>
2024-12-10 17:30:27 +0530

    Started PizzaMenu Project

--- PizzaMenu 03/pizza/src/App.jsx
+++ PizzaMenu 03/pizza/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Pizza from "./Pizza"
 
 function App() {
   return (
     <>
       <h1>Hello React</h1>
+      <Pizza />
     </>
   );
 }
---
+++ PizzaMenu 03/pizza/src/Pizza.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Pizza() {
+  return (
+    <>
+    <img src="/spinaci.jpg" alt="Image of Spinaci" className='imagespizza' />
+    <h2>Pizza SPinachi</h2>
+    <p>Tomato, Onion</p>
+    </>
+  )
+}
