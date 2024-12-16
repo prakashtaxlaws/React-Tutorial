0d3c7c8526d82f290f6fd0ef16a94e042f578a1f
Prakash K <prakash@HQ.CLI.IN>
2024-12-16 17:29:54 +0530

    Started State

--- Section06-State/Steps/steps/src/App.jsx
+++ Section06-State/Steps/steps/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const messages = [
@@ -7,13 +7,35 @@ const messages = [
   "Invest your new income 🤑",
 ];
 
-// function mouseH(){
-//   onmouseover= alert("Previous")
-// }
+
 export default function App() {
-  const step = 1;
+  const [step, setStep] =useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  function handleNext(){
+    if(step <3){
+      
+      setStep(step + 1);
+    }
+  }
+  // function handlePrev(){
+  //   if(step > 1){
+  //     setStep(step - 1);
+
+  //   }
+  // }
+  function handlePrev(){
+    if(step > 1){
+      setStep(step - 1);
+    }
+  }
+ 
+
   return (
     <>
+    
+      <button className='close' onClick={()=>setIsOpen(!isOpen)}>&times;</button>
+    { isOpen && (
+      
     <div className='steps'>
       <div className="numbers">
       <div className={`${step >= 1 ? "active" : ""}`}>1</div>
@@ -24,13 +46,15 @@ export default function App() {
         Step {step}: {messages[step - 1]}
       </p>
       <div className="buttons">
-      <button style={{backgroundColor: "#7950f2", color: "#fff"}} >Previous</button>
-      <button style={{backgroundColor: "#7950f2", color: "#fff"}} >Next</button>
+      <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handlePrev}>Previous</button>
+      <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handleNext}>Next</button>
         
       {/* onMouseOver={() => alert("Previous")} */}
       {/* onClick={() => alert("Next")} */}
       </div>
-    </div>
+    </div>)
+    }
+    
     </>
   )
 }
